7c374a6a4709d22b3a40479da2d49923b1c2f375
thanhtuit96 <thanhtuit96@gmail.com>
2017-09-18 01:55:31 +0700

    new update

--- PreProcessing/ScaleImage.py
+++ PreProcessing/ScaleImage.py
@@ -9,9 +9,9 @@ class ScaleImage:
 		self.path=path
 	def readFile(self):
 		return [x for x in os.listdir(self.path) if re.match('.*\.[gif|png|jpeg|jpg]', x)]
-	def readImage(self,path):
-		return Image.open(path).convert('RGB')
+	def readImage(self,im):
+		return Image.open(self.path+im).convert('RGB')
 	def resizeImage(self,img):
 		return img.resize(SIZES,Image.ANTIALIAS)
 	def saveImage(self,path,img):
-			temp.save(path,quality=195)
+			img.save(path,quality=195)
--- initData.py
+++ initData.py
@@ -2,9 +2,22 @@ from PreProcessing.ScaleImage  import ScaleImage
 import os
 import os.path
 
-PATH='Data-Raw/'
+PATH = 'Data-Raw/'
+TRAIN = 'Train/'
+TEST = 'Test/'
 Labels = os.listdir(PATH)
-
-print "Sub: " , subfolders
-##im = ScaleImage('Data-Raw/Ambarella/')
-##files=im.readFile()
+for label in Labels:
+	print "PreProcessing Label:", label 
+	im = ScaleImage(PATH+label+'/')
+	files = im.readFile()
+	index=2
+	if len(files) > 0:
+		for file in files:
+			print "Processing file ", file 
+			img = im.readImage(file)
+			img = im.resizeImage(img)
+			if index % 2 == 0 :
+				im.saveImage(TRAIN + label + '/' + str(index/2)+'.jpg',img)
+			else: 
+				im.saveImage(TEST + label + '/' + str(index/2) + '.jpg',img)
+			index = index + 1
